6fa8fe2fb51307b488d54883fd8e07ab66b30c7b
A <al>
2019-08-28 09:29:45 -0300

    Revert "Inserção dos exercícios E, F, G e H"
    This reverts commit 3bf4c19c8cb45db6579ce80920a9b77da40ef742.

--- AulaRevisao/exE.py
+++
@@ -1,5 +0,0 @@
-hora = float(input("Quanto você ganha por hora de trabalho?  "))
-horasTotal = float(input("Quanto horas por mês você trabalha? "))
-
-salario = hora * horasTotal
-print("Seu salário esse mês será de "+ str(salario) + " reais")
--- AulaRevisao/exf.py
+++
@@ -1,7 +0,0 @@
-area = float(input("Qual o tamanho em metros quadrados da área a ser pintada? "))
-
-litrosUsados = area / 3
-custo = litrosUsados * 80
-
-print("Custará R$", float("{0:.2f}".format(custo)), " reais")
-
