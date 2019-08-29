2ab455c48d36624dca9c82e88542e587258d169a
A <al>
2019-08-28 22:54:41 -0300

    adiciona respostas dos problemas vistos em sala

---
+++ AulaRevisao/exc.py
@@ -0,0 +1,6 @@
+avaliacao1 = input(" Informe a nota da Avaliação 1: ")
+avaliacao2 = input(" Informe a nota da Avaliação 2: ")
+
+media = (float(avaliacao1) + float(avaliacao2)) / 2
+
+print("Média: " + str(media))
